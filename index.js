b212045888578d8b5dda95e76595e3259dfa2e25
Wazir Robinson <robinson82425@gmail.com>
2019-05-25 00:06:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = ` Let\'s talk about ${topic}.`;
+var conversation = ` Let's talk about ${topic}.`;
